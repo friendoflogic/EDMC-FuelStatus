ea161cc7e4e85ea390c5c4feb84b403bc2bdf29c
friendoflogic <meinbobo@gmail.com>
2025-11-24 21:24:11 -0700

    load.py
    Added a new function to get the max values from the load out. Falls back to just showing the current if the max capacity isn't found. Updated display with explicit formatting and the reserve on top (to match the in-game ui).

--- load.py
+++ load.py
@@ -17,6 +17,9 @@ status: Optional[tk.Label]
 
 main_tank: Optional[float] = None
 reservoir: Optional[float] = None
+# two new variables for holding the max values
+main_capacity: Optional[float] = None
+reservoir_capacity: Optional[float] = None
 
 def plugin_start3(plugin_dir: str) -> str:
   logger.debug('fuelstatus plugin loaded')
@@ -46,6 +49,18 @@ def dashboard_entry(cmdr: str, is_beta: bool, entry: Dict[str, Any]) -> None:
       reservoir = entry["Fuel"]["FuelReservoir"]
   update_status()
 
+# new function to get the max values from the load out
+def journal_entry(cmdr: str, is_beta: bool, system: str, station: str, entry: dict[str, Any], state: dict[str, Any]) -> None:
+  global main_capacity, reservoir_capacity
+  main_capacity = None
+  reservoir_capacity = None
+  if "FuelCapacity" in state:
+    if "Main" in state["FuelCapacity"]:
+      main_capacity = state["FuelCapacity"]["Main"]
+    if "Reserve" in state["FuelCapacity"]:
+      reservoir_capacity = state["FuelCapacity"]["Reserve"]
+  update_status()
+
 def update_status() -> None:
   global label, status
   label["text"] = f'{_("Fuel levels")}:'
@@ -57,4 +72,13 @@ def update_status() -> None:
       status["text"] = _("ERROR")
       logger.error("One of main tank and reservoir fuel levels is None, the other isn’t … WTF?")
   else:
-    status["text"] = f'{round(main_tank,3)} t ({_("main")}), {round(reservoir,3)} t ({_("reservoir")})'
+    # Original display
+    #status["text"] = f'{round(main_tank,3)} t ({_("main")}), {round(reservoir,3)} t ({_("reservoir")})'
+    # updated display with explicit formatting and the reserve on top (to match the in-game ui)
+    # if we didn't get the max capacity for both, fallback to the current values only
+    if main_capacity is None or reservoir_capacity is None:
+        status["text"] = f' {round(reservoir,2):0>4.2f} T ({_("rsrv")})\n {round(main_tank,2):0>4.2f} T ({_("main")})'
+    # otherwise, use it
+    else:
+        status["text"] = f'  {round(reservoir,2):0>4.2f}  /  {round(reservoir_capacity,2):0>4.2f} T ({_("rsrv")})\n {round(main_tank,2):0>4.2f} / {round(main_capacity,2):0>4.2f} T ({_("main")})'
+
